c5c6fd1b731c89f668259030b959a3532afc7655
Balakrishnan SuthinachandraBabu <balakrishnanfrmwr@gmail.com>
2025-10-16 13:31:57 -0400

    Added functions in c code file

--- Functions/Functions.c
+++ Functions/Functions.c
@@ -1,11 +1,4 @@
-/******************************************************************************
 
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby,
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 /*The Below code file mainly describes function types that could be useful in Embedded programming
 /*Those are Recursive, static and Inline via macros*/
 
